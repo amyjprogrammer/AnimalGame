b149d12e68e464a6b40633a3ad0625b7f830d2e4
amyjprogrammer <amyjprogrammer@gmail.com>
2021-12-27 18:33:18 -0500

    Add project files.

---
+++ AnimalGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AnimalGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new ConsoleGame();
+            game.Play();
+        }
+    }
+}
