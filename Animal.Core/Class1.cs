49d9250a6b357d8ee6f87953bf086ac0f1ffd5fb
amyjprogrammer <amyjprogrammer@gmail.com>
2021-12-28 19:16:51 -0500

    more planning

--- Animal.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Animal.Core
-{
-    public class Class1
-    {
-    }
-}
--- Utils/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Utils
-{
-    public static class Class1
-    {
-    }
-}
